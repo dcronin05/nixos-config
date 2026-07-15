4006e1f2ae125cfdf49cf6f3375070b54baae8e1
dcronin05 <daniel@dcron.in>
2026-07-15 03:55:35 +0000

    chore: bump channel to nixos-unstable to support sops

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   description = "Cronin NixOS Infrastructure";
 
   inputs = {
-    # Using the stable 24.05 release channel
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # Using the rolling unstable release channel
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, sops-nix }: {
