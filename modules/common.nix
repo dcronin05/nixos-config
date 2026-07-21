8730bf7fa045a0a4bab40548cca61ad9bc93bcf3
dcronin05 <daniel@dcron.in>
2026-07-21 06:23:07 +0000

    feat: configure ssh matchBlocks and set default terminal environment variable

--- modules/common.nix
+++ modules/common.nix
@@ -89,7 +89,54 @@
       enable = true;
       nix-direnv.enable = true;
     };
-    programs.ssh.enable = true;
-    programs.ssh.enableDefaultConfig = false;
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+      matchBlocks = {
+        "*" = {
+          extraOptions = {
+            SetEnv = "TERM=xterm-256color";
+          };
+        };
+        "debian-vm" = {
+          hostname = "100.125.115.8";
+          user = "dcronin05";
+        };
+        "tower" = {
+          hostname = "100.79.77.74";
+          user = "dcron";
+        };
+        "macmini" = {
+          hostname = "100.117.198.24";
+          user = "dcronin05";
+        };
+        "macbook" = {
+          hostname = "100.76.154.2";
+          user = "dcronin05";
+        };
+        "gpantz" = {
+          hostname = "gpantz.castor.usbx.me";
+          user = "gpantz";
+        };
+        "pxe" = {
+          hostname = "dcron.in";
+          user = "root";
+          port = 2206;
+        };
+        "wsl" = {
+          hostname = "100.79.77.74";
+          user = "dcronin05";
+          port = 2299;
+        };
+        "vps" = {
+          hostname = "162.212.157.166";
+          user = "dcronin05";
+        };
+        "nix" = {
+          hostname = "192.168.1.170";
+          user = "dcronin05";
+        };
+      };
+    };
   };
 }
